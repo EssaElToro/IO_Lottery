0b4f0e1c5504da65d41bc82df594d1bac311e792
Igor Niewiadomski <96412905+EssaElToro@users.noreply.github.com>
2023-05-31 11:17:55 +0200

    Delete IO_LOTERRYPB-main directory

--- IO_LOTERRYPB-main/flask/domain.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class User:
-    first_name: str
--- IO_LOTERRYPB-main/tests/unit/test_views.py
+++
@@ -1,5 +0,0 @@
-from IO_LOTTERYPB.views import UserView
-
-
-def test_can_instantiate_user_view() -> None:
-    view = UserView()
